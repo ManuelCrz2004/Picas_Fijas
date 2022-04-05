383886ac380d82eb792785c4543c1f626736a738
Manuel Cruz G <manuelcruzg78@gmail.com>
2022-04-04 21:11:11 -0500

    Se agregan funciones adicionales de deploy y obt ener el int del ususario, se procedera a estructurar y dar estilo al html y css para que se pueda terminar el codigo JS

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 var RandomNumber;
+function deployGame(vidas) {
+    while (vidas > 0) {
+        GetRandomNumber();
+    }
+}
 function GetRandomNumber() {
     RandomNumber = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
-    if (RandomNumber.length == 3){
+    if (RandomNumber.length == 2){
         GetRandomNumber()
     } else {
         RandomNumber = parseInt(RandomNumber)
     }    
 }
+function getUserNumber() {
+    var userNumber = document.getElementById('')
+}
+function compareNumbers() {
 
+}
 
 function StartGame() {
+    var vidas = 10;
+    deployGame(vidas)
     GetRandomNumber();
     alert("El numero aleatorio es: " + RandomNumber)
 }  
